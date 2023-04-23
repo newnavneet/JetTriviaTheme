72a233861412d8abe17f02ecd62ec37ab044a04c
Nav511 <125399571+Nav511@users.noreply.github.com>
2023-04-23 16:02:19 +0530

    internal 2

--- app/src/main/java/com/example/jettriviatheme/MainActivity.kt
+++ app/src/main/java/com/example/jettriviatheme/MainActivity.kt
@@ -1,43 +1,68 @@
 package com.example.jettriviatheme
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
+
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.ViewModel
+import com.example.jettriviatheme.model.Question
+import com.example.jettriviatheme.screens.QuestionsViewModel
 import com.example.jettriviatheme.ui.theme.JetTriviaThemeTheme
+import dagger.hilt.android.AndroidEntryPoint
+import dagger.hilt.android.lifecycle.HiltViewModel
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetTriviaThemeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(color = MaterialTheme.colors.background) {
+
+                    // A surface container using the 'background' color from the theme
+
+
                 }
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+    @Composable
+    fun MyComposable(
+        viewModel: QuestionsViewModel = androidx.lifecycle.viewmodel.compose.viewModel()
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    JetTriviaThemeTheme {
-        Greeting("Android")
+    ) {
+
     }
+
+    @Composable
+
+    fun TriviaHome(viewModel: QuestionsViewModel ) {
+        Questions(viewModel)
+    }
+
+    @Composable
+    fun Questions(viewModel: QuestionsViewModel) {
+        val questions = viewModel.data.value.data?.toMutableList()
+        Log.d("TAG", "Questions: ${questions?.size}")
+    }
+
+
+    @Preview()
+    @Composable
+    fun DefaultPreview() {
+        JetTriviaThemeTheme {
+
+
+        }
+    }
+
 }
